939b680d0a742f99434658b5c80dbf0eb7ff7721
Ierga <i.ergakoff@gmail.com>
2024-01-22 23:05:05 +0300

    GB - Java Junior. Homework 4. Final

--- src/main/java/ru/gb/ergakov/Lesson4/Homework4/Hibernate/Program.java
+++ src/main/java/ru/gb/ergakov/Lesson4/Homework4/Hibernate/Program.java
@@ -5,54 +5,39 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import ru.gb.ergakov.Lesson4.Homework4.UI.ConsoleView;
 import ru.gb.ergakov.Lesson4.Homework4.models.Course;
-import ru.gb.ergakov.Lesson4.Seminar4.models.Student;
+
 
 public class Program {
 
     private static final ConsoleView cv = new ConsoleView();
+
     public static void main(String[] args) {
 
-        // Создание фабрики сессий
-        SessionFactory sessionFactory = new Configuration()
-                .configure("hibernate2.cfg.xml")
-                .addAnnotatedClass(Course.class)
-                .buildSessionFactory();
+            // Создание фабрики сессий
+            SessionFactory sessionFactory = new Configuration()
+                    .configure("hibernate2.cfg.xml")
+                    .addAnnotatedClass(Course.class)
+                    .buildSessionFactory();
 
-        // Создание сессии
-        try (Session session = sessionFactory.getCurrentSession()){
-
-            // Начало транзакции
-            session.beginTransaction();
-
-//            // Создание объекта
-//            Course course = new Course(cv.inputTitle(), cv.inputDuration());
-//
-//            // Сохранение объекта в базе данных
-//            session.save(course);
-//            System.out.println("Object course save successfully");
-
-            // Чтение объекта из базы данных
-            Course retrievedCourse = session.get(Course.class, cv.inputID());
-            System.out.println(retrievedCourse);
-
-            System.out.println("Object course retrieved successfully");
-            System.out.println("Retrieved course object: " + retrievedCourse);
-
-            // Обновление данных (Изменение продолжительности курса)
-//            retrievedCourse.setDuration(cv.inputDuration());
-//            session.update(retrievedCourse);
-//            System.out.println("Object student update successfully");
-
-            // Удаление объекта
-            session.delete(retrievedCourse);
-            System.out.println("Object student delete successfully");
-
-            // Коммит транзакции
-            session.getTransaction().commit();
-            System.out.println("Transaction commit successfully");
-
+            while (true) {
+            // Создание сессии
+            try (Session session = sessionFactory.getCurrentSession()) {
+                switch (cv.menu()) {
+                    // Вставка данных
+                    case 1 -> HibernateActions.insertData(session);
+                    // Чтение данных
+                    case 2 -> HibernateActions.showCourses(session);
+                    // Изменение названия курса
+                    case 3 -> HibernateActions.updateTitle(session);
+                    // Изменение продолжительности курса
+                    case 4 -> HibernateActions.updateDuration(session);
+                    // Удаление данных
+                    case 5 -> HibernateActions.deleteData(session);
+                    // Закрытие приложения
+                    case 0 -> System.exit(0);
+                    default -> System.out.println("Некорректный ввод");
+                }
+           }
         }
-
     }
-
 }
--- src/main/java/ru/gb/ergakov/Lesson4/Homework4/JDBC/Program.java
+++ src/main/java/ru/gb/ergakov/Lesson4/Homework4/JDBC/Program.java
@@ -2,12 +2,9 @@ package ru.gb.ergakov.Lesson4.Homework4.JDBC;
 
 import ru.gb.ergakov.Lesson4.Homework4.UI.ConsoleView;
 import ru.gb.ergakov.Lesson4.Homework4.models.Course;
-import ru.gb.ergakov.Lesson4.Seminar4.models.Student;
 
 import java.sql.*;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Random;
 
 public class Program {
     private static final ConsoleView cv = new ConsoleView();
@@ -18,118 +15,91 @@ public class Program {
         String password = "1234";
 
         try {
-            // Подключение к базе данных
-            Connection connection = DriverManager.getConnection(url, user, password);
+            while (true) {
+                // Подключение к базе данных
+                Connection connection = DriverManager.getConnection(url, user, password);
+                try (connection) {
+                    // Создание базы данных
+                    JDBCActions.createDatabase(connection);
+                    System.out.println("Database created successfully");
 
-            // Создание базы данных
-            createDatabase(connection);
-            System.out.println("Database created successfully");
+                    // Использование базы данных
+                    JDBCActions.useDatabase(connection);
+                    System.out.println("Use database successfully");
 
-            // Использование базы данных
-            useDatabase(connection);
-            System.out.println("Use database successfully");
+                    // Создание таблицы
+                    JDBCActions.createTable(connection);
+                    System.out.println("Create table successfully");
 
-            // Создание таблицы
-            createTable(connection);
-            System.out.println("Create table successfully");
+                    Collection<Course> courses = JDBCActions.readData(connection);
 
-            // Вставка данных
-//            insertData(connection, new Course(cv.inputTitle(), cv.inputDuration()));
-//            System.out.println("Insert data successfully");
-
-            // Чтение данных
-            Collection<Course> courses = readData(connection);
-            for (var course : courses)
-                System.out.println(course);
-            System.out.println("Read data successfully");
-
-            // Обновление данных (Изменение продолжительности курса)
-//            int findID = cv.inputID();
-//            for (var course : courses) {
-//                if (course.getId() == findID) course.setDuration(cv.inputDuration());
-//                updateData(connection, course);
-//            }
-//            System.out.println("Update data successfully");
-
-            // Удаление данных
-            deleteData(connection, cv.inputID());
-            System.out.println("Delete data successfully");
-
-            // Закрытие соединения
-            connection.close();
-            System.out.println("Database connection close successfully");
-
+                    switch (cv.menu()) {
+                        // Вставка данных
+                        case 1 -> insert(connection);
+                        // Чтение данных
+                        case 2 -> read(courses);
+                        // Изменение названия курса
+                        case 3 -> changeTitle(connection, courses);
+                        // Изменение продолжительности курса
+                        case 4 -> changeDuration(connection, courses);
+                        // Удаление данных
+                        case 5 -> delete(connection);
+                        // Закрытие приложения
+                        case 0 -> {
+                            connection.close();
+                            System.out.println("Database connection close successfully");
+                            System.exit(0);
+                        }
+                        default -> System.out.println("Некорректный ввод");
+                    }
+                } catch (RuntimeException e) {
+                    System.out.println("Некорректный ввод");
+                }
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
     }
 
-    //region Вспомогательные методы
+    // region Вспомогательные методы
 
-    private static void createDatabase(Connection connection) throws SQLException {
-        String createDatabaseSQL = "CREATE DATABASE IF NOT EXISTS schoolDB;";
-        PreparedStatement statement = connection.prepareStatement(createDatabaseSQL);
-        statement.execute();
+    // Вставка данных
+    private static void insert(Connection connection) throws SQLException {
+        JDBCActions.insertData(connection, new Course(cv.inputTitle(), cv.inputDuration()));
+        System.out.println("Insert data successfully");
     }
 
-    private static void useDatabase(Connection connection) throws SQLException {
-        String useDatabaseSQL = "USE schoolDB;";
-        try (PreparedStatement statement = connection.prepareStatement(useDatabaseSQL)) {
-            statement.execute();
+    // Чтение данных
+    private static void read(Collection<Course> courses) {
+        for (var course : courses)
+            System.out.println(course);
+        System.out.println("Read data successfully");
+    }
+
+    // Изменение названия курса
+    private static void changeTitle(Connection connection, Collection<Course> courses) throws SQLException {
+        int findID = cv.inputID();
+        for (var course : courses) {
+            if (course.getId() == findID) course.setTitle(cv.inputTitle());
+            JDBCActions.updateData(connection, course);
         }
+        System.out.println("Update data successfully");
     }
 
-    private static void createTable(Connection connection) throws SQLException {
-        String createTableSQL = "CREATE TABLE IF NOT EXISTS courses (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(30), duration INT);";
-        try (PreparedStatement statement = connection.prepareStatement(createTableSQL)) {
-            statement.execute();
+    // Изменение продолжительности курса
+    private static void changeDuration(Connection connection, Collection<Course> courses) throws SQLException {
+        int findID = cv.inputID();
+        for (var course : courses) {
+            if (course.getId() == findID) course.setDuration(cv.inputDuration());
+            JDBCActions.updateData(connection, course);
         }
+        System.out.println("Update data successfully");
     }
 
-    private static void insertData(Connection connection, Course course) throws SQLException {
-        String insertDataSQL = "INSERT INTO courses (title, duration) VALUES (?, ?);";
-        try (PreparedStatement statement = connection.prepareStatement(insertDataSQL)) {
-            statement.setString(1, course.getTitle());
-            statement.setInt(2, course.getDuration());
-            statement.executeUpdate();
-        }
+    // Удаление данных
+    private static void delete(Connection connection) throws SQLException {
+        JDBCActions.deleteData(connection, cv.inputID());
+        System.out.println("Delete data successfully");
     }
-
-    private static Collection<Course> readData(Connection connection) throws SQLException {
-        ArrayList<Course> courseList = new ArrayList<>();
-        String readDataSQL = "SELECT * FROM courses;";
-        try (PreparedStatement statement = connection.prepareStatement(readDataSQL)) {
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String title = resultSet.getString("title");
-                int duration = resultSet.getInt("duration");
-                courseList.add(new Course(id, title, duration));
-            }
-            return courseList;
-        }
-    }
-
-    private static void updateData(Connection connection, Course course) throws SQLException {
-        String updateDataSQL = "UPDATE courses SET title=?, duration=? WHERE id=?;";
-        try (PreparedStatement statement = connection.prepareStatement(updateDataSQL)) {
-            statement.setString(1, course.getTitle());
-            statement.setInt(2, course.getDuration());
-            statement.setInt(3, course.getId());
-            statement.executeUpdate();
-        }
-    }
-
-    private static void deleteData(Connection connection, int id) throws SQLException {
-        String deleteDataSQL = "DELETE FROM courses WHERE id=?;";
-        try (PreparedStatement statement = connection.prepareStatement(deleteDataSQL)) {
-            statement.setLong(1, id);
-            statement.executeUpdate();
-        }
-    }
-
-    //endregion
-
-
+    // endregion
 }
